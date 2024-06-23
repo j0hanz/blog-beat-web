a817c15c55f8f7355d7fa5cf692ad9c26a2780b3
Linus Johansson <golfarenlj@gmail.com>
2024-06-23 23:14:59 +0000

    Fix social Links icons responsiveness at smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,15 +12,15 @@ import styles from './styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={`footer bg-dark text-white py-2 ${styles.footer}`}>
-      <Container fluid>
+      <Container>
         <Row className="align-items-center">
-          <Col className="px-1">
+          <Col className="px-0">
             <p className="mb-0 text-white-50">
               <FontAwesomeIcon className="fa-sm px-1" icon={faCopyright} />
               2024 Blog Beat
             </p>
           </Col>
-          <Col className="px-1 text-end">
+          <Col className="px-0 text-end">
             <div className={`fa-lg ${styles.socialLinks}`}>
               {[
                 {
@@ -45,7 +45,6 @@ const Footer = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label={label}
-                  className="px-2"
                 >
                   <FontAwesomeIcon icon={icon} />
                 </a>
