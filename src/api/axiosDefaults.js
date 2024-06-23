b057ecaa7561a839e38113b82e3b809050fc499f
Linus Johansson <golfarenlj@gmail.com>
2024-06-23 20:55:39 +0000

    Configure Axios defaults for API requests

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://blog-beat-api-bab609deb9ee.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
