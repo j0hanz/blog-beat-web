e4190c9c681b6585dd1d5d36ee3d1bb70984bd8a
Linus Johansson <golfarenlj@gmail.com>
2024-06-23 20:56:41 +0000

    Add Footer component with modular styles and social media links

---
+++ src/components/Footer.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import styles from './styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={`footer bg-dark text-white py-2 ${styles.footer}`}>
+      <Container fluid>
+        <Row className="align-items-center">
+          <Col className="px-1">
+            <p className="mb-0 text-white-50">
+              <FontAwesomeIcon className="fa-sm px-1" icon={faCopyright} />
+              2024 Blog Beat
+            </p>
+          </Col>
+          <Col className="px-1 text-end">
+            <div className={`fa-lg ${styles.socialLinks}`}>
+              {[
+                {
+                  href: 'https://www.facebook.com',
+                  icon: faFacebook,
+                  label: 'Facebook',
+                },
+                {
+                  href: 'https://www.github.com',
+                  icon: faGithub,
+                  label: 'GitHub',
+                },
+                {
+                  href: 'https://www.instagram.com',
+                  icon: faInstagram,
+                  label: 'Instagram',
+                },
+              ].map(({ href, icon, label }, index) => (
+                <a
+                  key={index}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={label}
+                  className="px-2"
+                >
+                  <FontAwesomeIcon icon={icon} />
+                </a>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
