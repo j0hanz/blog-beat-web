70f04b3ad2c19e7b0553117361e76cbbf6d021f6
Linus Johansson <golfarenlj@gmail.com>
2024-06-25 10:38:41 +0000

    Add user context to manage current user state.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './api/axiosDefaults';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Post from './pages/NewPost';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/SignUp';
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./api/axiosDefaults";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css";
+import axios from "axios";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Post from "./pages/NewPost";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/SignUp";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    handleMount();
+  }, []);
   return (
-    <Router>
-      <div className={styles.App}>
-        <NavBar />
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/newpost" element={<Post />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <Router>
+          <div className={styles.App}>
+            <NavBar />
+            <main className={styles.main}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/newpost" element={<Post />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
