dd20344e111d594788e3c0adc6a1ae8b3facfaea
Linus Johansson <golfarenlj@gmail.com>
2024-06-25 23:02:13 +0000

    corrected jwtDecode import.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { axiosReq } from '../api/axiosDefaults';
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -18,32 +18,38 @@ export const fetchMoreData = async (resource, setResource) => {
 
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
-    ? {
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
         ...profile,
         followers_count: profile.followers_count + 1,
         following_id,
       }
     : profile.is_owner
-    ? {
-        ...profile,
-        following_count: profile.following_count + 1,
-      }
-    : profile;
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count + 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
 };
 
 export const unfollowHelper = (profile, clickedProfile) => {
   return profile.id === clickedProfile.id
-    ? {
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
         ...profile,
         followers_count: profile.followers_count - 1,
         following_id: null,
       }
     : profile.is_owner
-    ? {
-        ...profile,
-        following_count: profile.following_count - 1,
-      }
-    : profile;
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count - 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
 };
 
 export const setTokenTimestamp = (data) => {
