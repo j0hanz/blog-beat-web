c3faa0ac83549d2335d790e7825e05137e8b2da3
Linus Johansson <golfarenlj@gmail.com>
2024-06-25 11:21:38 +0000

    Create axios instances for requests and responses.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://blog-beat-api-bab609deb9ee.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
