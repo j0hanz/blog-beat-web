d903837e7ae26245dae60898b344a03e4e2e5525
Linus Johansson <golfarenlj@gmail.com>
2024-06-25 11:23:22 +0000

    Implement context and interceptors for user authentication.

---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,75 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+import { useNavigate } from 'react-router-dom';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get('dj-rest-auth/user/');
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    handleMount();
+  }, []);
+  useEffect(() => {
+    const requestInterceptor = axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post('/dj-rest-auth/token/refresh/');
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              navigate('/signin');
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      },
+    );
+    const responseInterceptor = axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/');
+            return axios(err.config);
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate('/signin');
+              }
+              return null;
+            });
+          }
+        }
+        return Promise.reject(err);
+      },
+    );
+    return () => {
+      axiosReq.interceptors.request.eject(requestInterceptor);
+      axiosRes.interceptors.response.eject(responseInterceptor);
+    };
+  }, [navigate]);
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
