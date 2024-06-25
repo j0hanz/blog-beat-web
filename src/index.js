6986c616c1f7dfe80857394fe3871abfb33c68d1
Linus Johansson <golfarenlj@gmail.com>
2024-06-25 18:38:34 +0000

    Add Router and CurrentUserProvider.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,9 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <CurrentUserProvider>
+      <App />
+    </CurrentUserProvider>
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
