54dbb373585f8a5167eaec0ba067598b892bcb5f
Linus Johansson <golfarenlj@gmail.com>
2024-06-26 00:06:33 +0000

    Add useRedirect.

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Custom hook to handle redirecting based on user authentication status
+export const useRedirect = (userAuthStatus) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        await axios.post('/dj-rest-auth/token/refresh/');
+        // if user is logged in, the code below will run
+        if (userAuthStatus === 'loggedIn') {
+          navigate('/');
+        }
+      } catch (err) {
+        // if user is not logged in, the code below will run
+        if (userAuthStatus === 'loggedOut') {
+          navigate('/');
+        }
+      }
+    };
+
+    handleMount();
+  }, [navigate, userAuthStatus]);
+};
