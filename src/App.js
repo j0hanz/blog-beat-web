cba02d9bc8507eee8e376d049a05390687da4a6f
Linus Johansson <golfarenlj@gmail.com>
2024-06-27 12:00:39 +0000

    Update routes and rename components in App.js.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import './api/axiosDefaults';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Post from './pages/NewPost';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/SignUp';
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./api/axiosDefaults";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import PostCreateForm from "./pages/PostCreateForm";
+import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+
   return (
     <div className={styles.App}>
       <NavBar />
@@ -18,9 +21,21 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/newpost" element={<Post />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/newpost"
+            element={
+              currentUser ? <PostCreateForm /> : <Navigate to="/signin" />
+            }
+          />
+          <Route
+            path="/signin"
+            element={!currentUser ? <SignInForm /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!currentUser ? <SignUpForm /> : <Navigate to="/" />}
+          />
+          <Route path="*" element={<p>Page not found!</p>} />
         </Routes>
       </main>
       <Footer />
