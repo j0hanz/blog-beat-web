4dabc795ead38c590e1300a5d73addef20f0ed68
Linus Johansson <golfarenlj@gmail.com>
2024-06-27 12:38:36 +0000

    Troubleshoot redirect issues.

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,20 +1,24 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
-import { axiosReq, axiosRes } from "../api/axiosDefaults";
-import { useNavigate } from "react-router-dom";
-import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+import { useNavigate } from 'react-router-dom';
+import { removeTokenTimestamp, shouldRefreshToken } from '../utils/utils';
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
+
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+// Provider component for managing the current user state
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
 
+  // Fetch the current user data on component mount
   const handleMount = async () => {
     try {
-      const { data } = await axiosRes.get("/dj-rest-auth/user/");
+      const { data } = await axiosRes.get('dj-rest-auth/user/');
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
@@ -25,44 +29,52 @@ export const CurrentUserProvider = ({ children }) => {
     handleMount();
   }, []);
 
-  useEffect(() => {
-    const requestInterceptor = axiosReq.interceptors.request.use(
+  useMemo(() => {
+    // Intercept requests to check for token expiration and refresh if necessary
+    axiosReq.interceptors.request.use(
       async (config) => {
         if (shouldRefreshToken()) {
           try {
-            await axios.post("/dj-rest-auth/token/refresh/");
+            await axios.post('/dj-rest-auth/token/refresh/');
           } catch (err) {
-            setCurrentUser(null);
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate('/signin');
+              }
+              return null;
+            });
             removeTokenTimestamp();
-            navigate("/signin");
+            return config;
           }
         }
         return config;
       },
-      (err) => Promise.reject(err)
+      (err) => {
+        return Promise.reject(err);
+      },
     );
 
-    const responseInterceptor = axiosRes.interceptors.response.use(
+    // Intercept responses to check for token expiration and refresh if necessary
+    axiosRes.interceptors.response.use(
       (response) => response,
       async (err) => {
         if (err.response?.status === 401) {
           try {
-            await axios.post("/dj-rest-auth/token/refresh/");
+            await axios.post('/dj-rest-auth/token/refresh/');
             return axios(err.config);
-          } catch (refreshErr) {
-            setCurrentUser(null);
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate('/login');
+              }
+              return null;
+            });
             removeTokenTimestamp();
-            navigate("/signin");
           }
         }
         return Promise.reject(err);
-      }
+      },
     );
-
-    return () => {
-      axiosReq.interceptors.request.eject(requestInterceptor);
-      axiosRes.interceptors.response.eject(responseInterceptor);
-    };
   }, [navigate]);
 
   return (
