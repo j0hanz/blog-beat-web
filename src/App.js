6bba19260e19010cd7cd1b095bb3d5134ac47eef
Linus Johansson <golfarenlj@gmail.com>
2024-06-28 16:44:28 +0000

    Add Notifications component and react-toastify CSS to App component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './api/axiosDefaults';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -9,6 +11,7 @@ import About from './pages/About';
 import PostCreateForm from './pages/PostCreateForm';
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
+import Notifications from './components/Notifications';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Notifications />
       <main className={styles.main}>
         <Routes>
           <Route path="/" element={<Home />} />
