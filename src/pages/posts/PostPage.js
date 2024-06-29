94994523f1ac0cafd51ed85cfa1d93f4790553f6
Linus Johansson <golfarenlj@gmail.com>
2024-06-29 20:41:26 +0000

    Add PostPage component to fetch and display individual post details.

---
+++ src/pages/posts/PostPage.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Col, Row, Container } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import styles from './styles/PostPage.module.css';
+import { axiosReq } from '../../api/axiosDefaults';
+import Post from './Post';
+
+function PostPage() {
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  return (
+    <Container
+      className={`${styles.Container} d-flex justify-content-center align-items-center`}
+      fluid
+    >
+      <Row className="h-100 w-100">
+        <Col className="py-2 p-0 p-lg-2 d-flex justify-content-center align-items-center">
+          {post.results.length > 0 ? (
+            <Post {...post.results[0]} setPosts={setPost} postPage />
+          ) : (
+            <p>Loading...</p>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default PostPage;
