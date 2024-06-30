6cc1b5421bd50eed997b7ca9f7097f940141cec7
Linus Johansson <golfarenlj@gmail.com>
2024-06-30 21:50:02 +0000

    Add comment form and render comments on PostPage.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -3,19 +3,26 @@ import { Col, Row, Container } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { axiosReq } from '../../api/axiosDefaults';
 import Post from './Post';
+import CommentCreateForm from '../comments/CommentCreateForm';
+import Comment from '../comments/Comment';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -27,12 +34,40 @@ function PostPage() {
   return (
     <Container
       fluid
-      className="d-flex justify-content-center align-items-center"
+      className="d-flex flex-column justify-content-center align-items-center"
     >
       <Row className="h-100 w-100">
-        <Col className="py-2 p-0 p-lg-2 d-flex justify-content-center align-items-center">
+        <Col className="py-2 p-0 p-lg-2 d-flex flex-column justify-content-center align-items-center">
           {post.results.length > 0 ? (
-            <Post {...post.results[0]} setPosts={setPost} postPage />
+            <>
+              <Post {...post.results[0]} setPosts={setPost} postPage />
+              <Container className="mt-4">
+                {currentUser ? (
+                  <CommentCreateForm
+                    profile_id={currentUser.profile_id}
+                    profileImage={profile_image}
+                    post={id}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ) : comments.results.length ? (
+                  'Comments'
+                ) : null}
+                {comments.results.length ? (
+                  comments.results.map((comment) => (
+                    <Comment key={comment.id} {...comment} />
+                  ))
+                ) : (
+                  <div className="d-flex justify-content-center mt-3">
+                    {currentUser ? (
+                      <span>No comments yet, be the first to comment!</span>
+                    ) : (
+                      <span>No comments... yet</span>
+                    )}
+                  </div>
+                )}
+              </Container>
+            </>
           ) : (
             <p>Loading...</p>
           )}
