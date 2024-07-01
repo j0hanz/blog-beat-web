06a1ef769dd2c2b01f58446dddf49347eb3f3dce
Linus Johansson <golfarenlj@gmail.com>
2024-07-01 23:59:43 +0000

    Add VerticalThreeDots component for rendering a vertical ellipsis icon with a button outline and click handling.

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -5,6 +5,9 @@ import {
   faEllipsis,
   faEdit,
   faTrashAlt,
+  faEllipsisVertical,
+  faIdCard,
+  faKey,
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles/MoreDropdown.module.css';
 import { useNavigate } from 'react-router-dom';
@@ -13,6 +16,18 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   <FontAwesomeIcon
     className="btn btn-outline-light p-1"
     icon={faEllipsis}
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+const VerticalThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <FontAwesomeIcon
+    className="btn btn-outline-light p-1"
+    icon={faEllipsisVertical}
     ref={ref}
     onClick={(e) => {
       e.preventDefault();
@@ -55,34 +70,43 @@ export const ProfileEditDropdown = ({ id }) => {
 
   return (
     <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
-      <Dropdown.Toggle as={ThreeDots} />
-      <Dropdown.Menu>
+      <Dropdown.Toggle as={VerticalThreeDots} />
+      <Dropdown.Menu className={`text-center ${styles.DropdownMenu}`}>
         <Dropdown.Item
+          className={styles.DropdownItem}
           onClick={() => {
             console.log(`Navigating to /profiles/${id}/edit`);
             navigate(`/profiles/${id}/edit`);
           }}
           aria-label="edit-profile"
         >
-          <i className="fas fa-edit" /> edit profile
+          <FontAwesomeIcon className={`fa-lg ${styles.faEdit}`} icon={faEdit} />
+          <span className={styles.DropdownItem}>edit profile</span>
         </Dropdown.Item>
         <Dropdown.Item
+          className={styles.DropdownItem}
           onClick={() => {
             console.log(`Navigating to /profiles/${id}/edit/username`);
             navigate(`/profiles/${id}/edit/username`);
           }}
           aria-label="edit-username"
         >
-          <i className="far fa-id-card" /> change username
+          <FontAwesomeIcon
+            className={`fa-lg ${styles.faIdCard}`}
+            icon={faIdCard}
+          />
+          <span className={styles.DropdownItem}>change username</span>
         </Dropdown.Item>
         <Dropdown.Item
+          className={styles.DropdownItem}
           onClick={() => {
             console.log(`Navigating to /profiles/${id}/edit/password`);
             navigate(`/profiles/${id}/edit/password`);
           }}
           aria-label="edit-password"
         >
-          <i className="fas fa-key" /> change password
+          <FontAwesomeIcon className={`fa-lg ${styles.faKey}`} icon={faKey} />
+          <span className={styles.DropdownItem}>change password</span>
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
