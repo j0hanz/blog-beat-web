40d50520b7a01b226f492f83031112120a25d211
Linus Johansson <golfarenlj@gmail.com>
2024-07-01 12:05:27 +0000

    Create a Profile component to display user profiles with follow/unfollow functionality.

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './styles/Profile.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from 'react-router-dom';
+import Icon from '../../components/Icon';
+import { Button } from 'react-bootstrap';
+
+const Profile = (props) => {
+  const { profile, imageSize = 45 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <div className="d-flex align-items-center">
+      <div className="d-flex flex-column align-items-center">
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Icon src={image} height={imageSize} />
+        </Link>
+        <strong className={`my-1 ${styles.WordBreak}`}>{owner}</strong>
+        {currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button variant="outline-primary btn-sm" onClick={() => {}}>
+              unfollow
+            </Button>
+          ) : (
+            <Button variant="outline-primary btn-sm" onClick={() => {}}>
+              follow
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
