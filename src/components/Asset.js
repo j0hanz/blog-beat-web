f4130f2c6d1ac606decfc3f4f2bdaf0b5a010ec1
Linus Johansson <golfarenlj@gmail.com>
2024-07-02 14:25:28 +0000

    Add style to spinner.

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Spinner } from "react-bootstrap";
-import styles from "./styles/Asset.module.css";
+import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import styles from './styles/Asset.module.css';
 
 const Asset = ({ spinner, src, message }) => {
   return (
-    <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+    <div className={`${styles.Asset} p-5`}>
+      {spinner && <Spinner animation="border" className={styles.spinner} />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
