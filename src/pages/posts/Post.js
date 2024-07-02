2e0e5b1f1898bfa594f58eb833deb525058a8442
Linus Johansson <golfarenlj@gmail.com>
2024-07-02 14:29:40 +0000

    Changed to bootstrap grid layout.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styles from './styles/Post.module.css';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import {
+  Card,
+  Col,
+  Container,
+  Row,
+  OverlayTrigger,
+  Tooltip,
+} from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import Icon from '../../components/Icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -76,34 +83,36 @@ const Post = (props) => {
   };
 
   return (
-    <div className="mx-auto mt-4">
-      <Card className={`bg-dark ${styles.Post}`}>
+    <Container className="mt-4">
+      <Card className={`${styles.Post}`}>
         <Card.Body>
-          <div className="d-flex align-items-center justify-content-between">
-            <Link
-              to={`/profiles/${profile_id}`}
-              className="d-flex align-items-center"
-            >
-              <Icon src={profile_image} height={35} />
-              <span>{owner}</span>
-            </Link>
-            <div className="d-flex align-items-center">
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <Link
+                to={`/profiles/${profile_id}`}
+                className="d-flex align-items-center"
+              >
+                <Icon src={profile_image} height={35} />
+                <span>{owner}</span>
+              </Link>
+            </Col>
+            <Col className="d-flex justify-content-end align-items-center">
               <span className="text-white-50 mx-2">{updated_at}</span>
               {is_owner && postPage && (
-                <div className={styles.Absolute}>
-                  <MoreDropdown
-                    handleEdit={handleEdit}
-                    handleDelete={handleDelete}
-                  />
-                </div>
+                <MoreDropdown
+                  handleEdit={handleEdit}
+                  handleDelete={handleDelete}
+                />
               )}
-            </div>
-          </div>
+            </Col>
+          </Row>
           <hr />
         </Card.Body>
-        <Link to={`/posts/${id}`}>
-          <Card.Img className={styles.CardImg} src={image} alt={title} />
-        </Link>
+        {image && (
+          <Link to={`/posts/${id}`}>
+            <Card.Img className={styles.CardImg} src={image} alt={title} />
+          </Link>
+        )}
         {title && <Card.Title className="text-center my-3">{title}</Card.Title>}
         <Card.Body>
           {content && <Card.Text>{content}</Card.Text>}
@@ -152,7 +161,7 @@ const Post = (props) => {
           </div>
         </Card.Body>
       </Card>
-    </div>
+    </Container>
   );
 };
 
