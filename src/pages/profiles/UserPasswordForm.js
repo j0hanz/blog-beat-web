2d9083a1746e98adc07ac3f0756265d544b15de9
Linus Johansson <golfarenlj@gmail.com>
2024-07-02 00:02:03 +0000

    Add modal for changing password with improved styles.

--- src/pages/profiles/UserPasswordForm.js
+++ src/pages/profiles/UserPasswordForm.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import { Alert, Button, Container, Row, Col, Form } from 'react-bootstrap';
+import { Alert, Button, Container, Form, Modal } from 'react-bootstrap';
 
 import { axiosRes } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import styles from './styles/ProfileEditForm.module.css';
 
 const UserPasswordForm = () => {
   const navigate = useNavigate();
@@ -27,18 +30,11 @@ const UserPasswordForm = () => {
   };
 
   useEffect(() => {
-    console.log(
-      `UserPasswordForm useEffect: Checking profile ownership for ID: ${id}`,
-    );
-    console.log(
-      `UserPasswordForm useEffect: Current User Profile ID: ${currentUser?.pk}`,
-    );
     if (id === undefined) {
       console.error('UserPasswordForm: ID is undefined!');
       return;
     }
     if (currentUser?.pk?.toString() !== id) {
-      // redirect user if they are not the owner of this profile
       navigate('/');
     }
   }, [currentUser, navigate, id]);
@@ -49,17 +45,32 @@ const UserPasswordForm = () => {
       await axiosRes.post('/dj-rest-auth/password/change/', userData);
       navigate(-1);
     } catch (err) {
-      console.log(err);
       setErrors(err.response?.data);
     }
   };
 
+  const handleClose = () => {
+    navigate(-1);
+  };
+
   return (
-    <Row>
-      <Col className="py-2 mx-auto text-center" md={6}>
-        <Container>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
+    <Modal show={true} onHide={handleClose} centered>
+      <Modal.Header
+        className={`d-flex justify-content-center p-3 ${styles.modalHeadBg} position-relative`}
+      >
+        <Modal.Title className="text-center">Change Password</Modal.Title>
+        <Button
+          variant="link"
+          className="text-white position-absolute end-0 top-0 btn-outline-secondary btn-sm"
+          onClick={handleClose}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </Button>
+      </Modal.Header>
+      <Modal.Body className={`text-center ${styles.modalBodyBg}`}>
+        <Form onSubmit={handleSubmit}>
+          <Container className="text-center">
+            <Form.Group className="mb-3">
               <Form.Label>New password</Form.Label>
               <Form.Control
                 placeholder="new password"
@@ -74,7 +85,7 @@ const UserPasswordForm = () => {
                 {message}
               </Alert>
             ))}
-            <Form.Group>
+            <Form.Group className="mb-3">
               <Form.Label>Confirm password</Form.Label>
               <Form.Control
                 placeholder="confirm new password"
@@ -89,12 +100,24 @@ const UserPasswordForm = () => {
                 {message}
               </Alert>
             ))}
-            <Button onClick={() => navigate(-1)}>cancel</Button>
-            <Button type="submit">save</Button>
-          </Form>
-        </Container>
-      </Col>
-    </Row>
+            <Button
+              variant="outline-warning"
+              onClick={handleClose}
+              className="mx-3 btn-lg"
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="outline-primary"
+              type="submit"
+              className="mx-3 btn-lg"
+            >
+              Save
+            </Button>
+          </Container>
+        </Form>
+      </Modal.Body>
+    </Modal>
   );
 };
 
