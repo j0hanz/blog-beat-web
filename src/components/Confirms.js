f9da7e4252c0aafc433a4fa246ae630ae3739724
Linus Johansson <golfarenlj@gmail.com>
2024-07-03 20:46:57 +0000

    Add confirmation modal with delete and cancel options.

---
+++ src/components/Confirms.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import styles from './styles/Confirms.module.css';
+
+const Confirm = ({ show, onConfirm, onCancel }) => {
+  return (
+    <Modal show={show} onHide={onCancel} centered>
+      <Modal.Header
+        className={`d-flex justify-content-center p-3 ${styles.modalBodyBg} position-relative`}
+      >
+        <Modal.Title className="text-center">Confirm Deletion</Modal.Title>
+        <Button
+          variant="link"
+          className="text-white position-absolute end-0 top-0 btn-outline-secondary btn-sm"
+          onClick={onCancel}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </Button>
+      </Modal.Header>
+      <Modal.Body className={`text-center ${styles.modalBodyBg}`}>
+        <p>Are you sure you want to delete?</p>
+        <div className="d-flex justify-content-center">
+          <Button
+            variant="outline-secondary"
+            className={`mx-2 ${styles.buttonOutlinePrimary}`}
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            className={`mx-2 ${styles.buttonOutlinePrimary}`}
+            onClick={onConfirm}
+          >
+            Delete
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default Confirm;
