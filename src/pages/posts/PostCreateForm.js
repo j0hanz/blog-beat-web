063a9e8e209ec2ef9b7e4d1bf5db97f2786ce9cf
Linus Johansson <golfarenlj@gmail.com>
2024-09-07 12:29:07 +0000

    Refactor code for improved readability and maintainability

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faFileAlt } from '@fortawesome/free-solid-svg-icons';
@@ -16,12 +16,10 @@ import Upload from '../../assets/upload.png';
 import styles from './styles/PostCreateForm.module.css';
 import Asset from '../../components/Asset';
 import { axiosReq } from '../../api/axiosDefaults';
-import { useRedirect } from '../../hooks/useRedirect';
 import { toast } from 'react-toastify';
 
-/* PostCreateForm component for creating a new post */
 function PostCreateForm() {
-  useRedirect('loggedOut');
+  // State for form errors and post data
   const [errors, setErrors] = useState({});
   const [postData, setPostData] = useState({
     title: '',
@@ -34,180 +32,136 @@ function PostCreateForm() {
   const imageInput = useRef(null);
   const navigate = useNavigate();
 
-  /* Handle input change */
-  const handleChange = (event) => {
-    setPostData({
-      ...postData,
-      [event.target.name]: event.target.value,
-    });
-  };
+  // Handle input changes
+  const handleChange = useCallback((event) => {
+    const { name, value } = event.target;
+    setPostData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }, []);
 
-  /* Handle image change */
-  const handleChangeImage = (event) => {
-    if (event.target.files.length) {
-      URL.revokeObjectURL(image);
-      setPostData({
-        ...postData,
-        image: URL.createObjectURL(event.target.files[0]),
-      });
-    }
-  };
+  // Handle image upload
+  const handleChangeImage = useCallback(
+    (event) => {
+      if (event.target.files.length) {
+        URL.revokeObjectURL(image);
+        const file = event.target.files[0];
+        const imageUrl = URL.createObjectURL(file);
 
-  /* Handle removing the image */
-  const handleRemoveImage = () => {
+        setPostData((prevData) => ({
+          ...prevData,
+          image: imageUrl,
+        }));
+      }
+    },
+    [image]
+  );
+
+  // Handle image removal
+  const handleRemoveImage = useCallback(() => {
     URL.revokeObjectURL(image);
-    setPostData({
-      ...postData,
+    setPostData((prevData) => ({
+      ...prevData,
       image: '',
-    });
+    }));
+
     if (imageInput.current) {
       imageInput.current.value = '';
     }
-  };
+  }, [image]);
 
-  /* Handle form submission */
+  // Handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-
     formData.append('title', title);
     formData.append('location', location);
     formData.append('content', content);
+
     if (imageInput.current.files[0]) {
       formData.append('image', imageInput.current.files[0]);
     }
 
     try {
       const { data } = await axiosReq.post('/posts/', formData);
-      toast('Post created successfully!');
+      toast.success('Post created successfully!');
       navigate(`/posts/${data.id}`);
     } catch (err) {
-      console.log(err);
+      console.error(err);
       if (err.response?.status !== 401) {
-        setErrors(err.response?.data);
+        setErrors(
+          err.response?.data || { general: ['An unexpected error occurred.'] }
+        );
         toast.error('Failed to create post. Please try again.');
       }
     }
   };
 
-  const textFields = (
-    <div className="text-center">
-      <Form.Group controlId="title" className="mb-3">
-        <Form.Label className="d-none">Title</Form.Label>
-        <InputGroup>
-          <InputGroup.Text>
-            <FontAwesomeIcon icon={faFileAlt} />
-          </InputGroup.Text>
-          <Form.Control
-            type="text"
-            placeholder="Title"
-            name="title"
-            value={title}
-            onChange={handleChange}
-            className={styles.FormControl}
-          />
-        </InputGroup>
-        {errors?.title?.map((message, idx) => (
-          <Alert variant="warning" key={idx}>
-            {message}
-          </Alert>
-        ))}
-      </Form.Group>
-
-      <Form.Group controlId="location" className="mb-3">
-        <Form.Label className="d-none">Location</Form.Label>
-        <InputGroup>
-          <InputGroup.Text>
-            <FontAwesomeIcon icon={faMapMarkerAlt} />
-          </InputGroup.Text>
-          <Form.Control
-            type="text"
-            placeholder="Location"
-            name="location"
-            value={location}
-            onChange={handleChange}
-            className={styles.FormControl}
-          />
-        </InputGroup>
-        {errors?.location?.map((message, idx) => (
-          <Alert variant="warning" key={idx}>
-            {message}
-          </Alert>
-        ))}
-      </Form.Group>
-
-      <Form.Group controlId="content" className="mb-4">
-        <Form.Label className="d-none">Content</Form.Label>
-        <InputGroup>
-          <InputGroup.Text>
-            <FontAwesomeIcon icon={faFileAlt} />
-          </InputGroup.Text>
-          <Form.Control
-            as="textarea"
-            rows={6}
-            placeholder="Content"
-            name="content"
-            value={content}
-            onChange={handleChange}
-            className={styles.FormControl}
-          />
-        </InputGroup>
-        {errors?.content?.map((message, idx) => (
-          <Alert variant="warning" key={idx}>
-            {message}
-          </Alert>
-        ))}
-      </Form.Group>
-
-      <Button
-        variant="outline-warning text-white"
-        onClick={() => navigate(-1)}
-        className="mx-3 btn-lg"
-      >
-        Cancel
-      </Button>
-      <Button variant="outline-primary" className="mx-3 btn-lg" type="submit">
-        Create
-      </Button>
-    </div>
+  // Render text input fields
+  const renderTextField = (name, placeholder, icon, as = 'input', rows) => (
+    <Form.Group controlId={name} className="mb-3">
+      <Form.Label className="d-none">{placeholder}</Form.Label>
+      <InputGroup>
+        <InputGroup.Text>
+          <FontAwesomeIcon icon={icon} />
+        </InputGroup.Text>
+        <Form.Control
+          as={as}
+          rows={rows}
+          type="text"
+          placeholder={placeholder}
+          name={name}
+          value={postData[name]}
+          onChange={handleChange}
+          className={styles.FormControl}
+        />
+      </InputGroup>
+      {errors[name]?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+    </Form.Group>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
       <Container
-        className={`${styles.Container} d-flex flex-column justify-content-center`}
+        className={`d-flex flex-column justify-content-center ${styles.Container}`}
       >
+        {/* Image upload section */}
         <Row>
           <Col className="py-2 p-0 p-md-2" md={12} lg={12}>
             <Form.Group className="text-center">
-              <div
-                className={styles.ImageWrapper}
-                onClick={() => imageInput.current.click()}
-              >
-                {image ? (
+              {image ? (
+                <div
+                  className={styles.ImageWrapper}
+                  onClick={() => imageInput.current.click()}
+                >
                   <figure>
-                    <Image className={styles.Image} src={image} rounded />
+                    <Image className={styles.Image} src={image} fluid rounded />
                     <div className={styles.Placeholder}>
                       Click to change the image
                     </div>
                   </figure>
-                ) : (
-                  <Form.Label
-                    className="d-flex justify-content-center"
-                    htmlFor="image-upload"
-                  >
-                    <Asset src={Upload} message="Click to upload an image" />
-                  </Form.Label>
-                )}
-                <Form.Control
-                  type="file"
-                  id="image-upload"
-                  accept="image/*"
-                  className="d-none"
-                  ref={imageInput}
-                  onChange={handleChangeImage}
-                />
-              </div>
+                </div>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset src={Upload} message="Click to upload an image" />
+                </Form.Label>
+              )}
+              <Form.Control
+                type="file"
+                id="image-upload"
+                accept="image/*"
+                className="d-none"
+                ref={imageInput}
+                onChange={handleChangeImage}
+              />
               {image && (
                 <div className="d-flex justify-content-center mt-2">
                   <Button
@@ -227,9 +181,28 @@ function PostCreateForm() {
             ))}
           </Col>
         </Row>
+        {/* Form fields */}
         <Row>
           <Col className="py-2 p-0 p-md-2" md={12} lg={12}>
-            {textFields}
+            <div className="text-center">
+              {renderTextField('title', 'Title', faFileAlt)}
+              {renderTextField('location', 'Location', faMapMarkerAlt)}
+              {renderTextField('content', 'Content', faFileAlt, 'textarea', 6)}
+              <Button
+                variant="outline-warning text-white"
+                onClick={() => navigate(-1)}
+                className="mx-3 btn-lg"
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="outline-primary"
+                className="mx-3 btn-lg"
+                type="submit"
+              >
+                Create
+              </Button>
+            </div>
           </Col>
         </Row>
       </Container>
